7941a8403f36946719e8b57cfae344efb0a79af7
blob1807 <12388588+blob1807@users.noreply.github.com>
2022-08-23 01:01:19 +1000

    Added main python script & README
    First commit of cc_date.py & README.md

--- README.md
+++ README.md
@@ -1,2 +1,290 @@
-# cc_date
+![Python-Ver]
+
+# Cosmic Critter Date Format Handler
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#date-terminology">Date Terminology</a></li>
+    <li>
+        <a href="#cli-usage">CLI Usage</a>
+        <ul>
+            <li><a href="#help">Help</a></li>
+            <li><a href="#add">Add</a></li>
+            <li><a href="#subtract">Subtract</a></li>
+            <li><a href="#multiple">Multiple</a></li>
+            <li><a href="#divide">Divide</a></li>
+            <li><a href="#valid-formats">Valid Formats</a></li>
+            <li><a href="#convert">Convert</a></li>
+            <li><a href="#calculate">Calculate</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#library-usage">Library Usage</a>
+        <ul>
+            <li><a href="#exaples">Examples</a> </li>
+        </ul>
+    </li>
+  </ol>
+</details>
+
+
+
+<!-- DATE TERMINOLOGY -->
+## Date Terminology
+* String   - !1234 ABC 123
+* Digits   - (1234, 0, 1, 2, 123)
+* Decimal  - 21688812123
+
+
+
+<!-- CLI Usage -->
+## CLI Usage
+
+For dates with Spaces use.<br>
+This will be used throughout this README
+
+*Windows*
+```commandline
+cc_date.py -a "!1234 ABC 123" "!4321 CBA 321"
+```
+
+*Linux*
+```commandline
+python3 cc_date.py -a "!1234 ABC 123" "!4321 CBA 321"
+```
+
+For dates without Spaces use.
+
+*Windows*
+```commandline
+cc_date.py -a !1234ABC123 !4321CBA321
+```
+
+*Linux*
+```commandline
+python3 cc_date.py -a !1234ABC123 !4321CBA321
+```
+<br>
+
+### Help
+Returns help
+
+**Windows**
+```commandline
+cc_date.py -h
+```
+**Linux**
+```commandline
+python3 cc_date.py -h
+```
+**Out**
+```
+usage: cc_date.py [-h]
+                  (-a | -s | -m | -d | -f | -c {string,digits,decimal,human} | -C {radix,weights})
+                  dates [dates ...]
+
 For use with Ivycomb's Cosmic Critters date format.
+
+positional arguments:
+  dates                 CC Date/s
+
+options:
+  -h, --help            show this help message and exit
+  -a, --add             Add 2 or more CC Dates
+  -s, --sub             Sub 2 or more CC Dates
+  -m, --multiple        Multiple 2 or more CC Dates
+  -d, --divide          Divide 2 or more CC Dates
+  -f, --valid-formats   Prints all valid input formats
+  -c {string,digits,decimal,human}, --convert {string,digits,decimal,human}
+                        Convert from String, Digits & Decimal Dates to String,
+                        Digits, Decimal or Human Dates
+  -C {radix,weights}, --calculate {radix,weights}
+                        Calculates the Radixes or Weights of given Date\s
+```
+<br>
+
+### Add
+Returns added dates. <br> dates1 + date2 + ...
+
+**Windows**
+```commandline
+cc_date.py -a "!1234 ABC 123" "!4321 CBA 321"
+```
+**Linux**
+```commandline
+python3 cc_date.py -a "!1234 ABC 123" "!4321 CBA 321"
+```
+**Out**
+```
+!!5555 CCC 444
+```
+<br>
+
+### Subtract
+Returns subtracted dates. <br> dates1 - date2 - ...
+
+**Windows**
+```commandline
+cc_date.py -a "!1234 ABC 123" "!4321 CBA 321"
+```
+**Linux**
+```commandline
+python3 cc_date.py -s "!1234 ABC 123" "!4321 CBA 321"
+```
+**Out**
+```
+!-3088 YAB 802
+```
+<br>
+
+### Multiple
+Returns multiplied dates. <br> dates1 x date2 x ...
+
+**Windows**
+```commandline
+cc_date.py -m "!1234 ABC 123" "!4321 CBA 321"
+```
+**Linux**
+```commandline
+python3 cc_date.py -m "!1234 ABC 123" "!4321 CBA 321"
+```
+**Out**
+```
+!93719058033802 LAF 483
+```
+<br>
+
+### Divide
+Returns floor divided dates. <br> dates1 / date2 / ...
+
+**Windows**
+```commandline
+cc_date.py -d "!1234 ABC 123" "!4321 CBA 321"
+```
+**Linux**
+```commandline
+python3 cc_date.py -d "!1234 ABC 123" "!4321 CBA 321"
+```
+**Out**
+```
+!0 AAA 0
+```
+<br>
+
+### Convert
+Convert from string, digits & decimal dates to string, digits, decimal & human dates
+
+**Windows**
+```commandline
+cc_date.py -c decimal "!1234 ABC 123" "!4321 CBA 321"
+```
+**Linux**
+```commandline
+python3 cc_date.py -c decimal "!1234 ABC 123" "!4321 CBA 321"
+```
+**Out**
+```
+Date\s converted to Decimal format:
+!1234 ABC 123 -> 21688812123
+!4321 CBA 321 -> 75947274321
+```
+<br>
+
+### Calculate
+Returns either the radix or weights of given dates.
+
+**Windows**
+```commandline
+cc_date.py -C radix "!1234 ABC 123" "!4321 CBA 321"
+```
+**Linux**
+```commandline
+python3 cc_date.py -C radix "!1234 ABC 123" "!4321 CBA 321"
+```
+**Out**
+```
+Place value Radixes:
+!1234 ABC 123 -> (10000, 26, 26, 26, 1000)
+!4321 CBA 321 -> (10000, 26, 26, 26, 1000)
+```
+<br>
+
+### Valid Formats
+Returns all valid inputs. Separated by `|`.
+
+**Windows**
+```commandline
+cc_date.py -f
+```
+**Linux**
+```commandline
+python3 cc_date.py -f
+```
+**Out**
+```
+Valid Input Formats:
+Strings & Decimal:
+!1234 aAa 123 | !1234aAa123 
+1234 aAa 123 | 1234aAa123
+1234 A 123 | A 123 | 1234 A 
+1234A123 | A123 | 1234A 
+234123 | aAa
+
+Digits:
+[0-inf, 0-26, 0-26, 0-26, ..., 0-999]
+```
+<br>
+
+
+
+<!-- Library Usage -->
+## Library Usage
+Coming Soon.<br>
+For now just look at the `cli()` func, docstrings & comments on how to use it.
+
+### Examples
+Get date in Decimal
+```python
+from cc_date import cc_date
+
+date = cc_date("!1234 ABC 123")
+print(cc_date.decimal)
+```
+```pycon
+21688812123
+```
+<br>
+
+Add dates.
+
+```python
+from cc_date import cc_date, cc_math
+
+date_1 = cc_date("!1234 ABC 123")
+date_2 = cc_date("!4321 CBA 321")
+date_3 = cc_math.add([date_1, date_2])
+
+print(date_3.string)
+```
+```pycon
+!5555 CCC 444
+```
+<br>
+
+Convert CC Date to Human Date
+```python
+from cc_date import cc_date
+from datetime import datetime
+
+date = cc_date("!1234 ABC 123")
+print(datetime.fromtimestamp(date.decimal))
+```
+```pycon
+2657-04-16 22:02:03
+```
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[Python-Ver]: https://img.shields.io/badge/python-3.10.x-blue?style=for-the-badge
---
+++ cc_date.py
@@ -0,0 +1,326 @@
+import re
+import dataclasses
+from string import ascii_uppercase
+
+__doc__ = """For use with Ivycomb's Cosmic Critters date format."""
+
+
+# ========== CC Date Format Class ============
+class cc_format:
+    """
+    Docstring to be implemented\n
+    -Blob
+     """
+    valid_string_formats = '!1234 aAa 123 | !1234aAa123 \n1234 aAa 123 | 1234aAa123\n' \
+                           '1234 A 123 | A 123 | 1234 A \n1234A123 | A123 | 1234A \n' \
+                           '234123 | aAa'
+    valid_digits_format = '[0-inf, 0-26, 0-26, 0-26, ..., 0-999]'
+    cc_format = '!1234 AAA 123'
+    regex = re.compile(r'!?\d+ ?[a-z]{3} ?\d{3}(?=\D|\Z)', re.IGNORECASE)
+    regex_greedy = re.compile(r'(\d*) *([a-z]+) *(\d*(?=\D|\Z))', re.IGNORECASE)
+
+    class normalize:
+        @staticmethod
+        def string(date: any) -> str:
+            work = list(cc_format.regex_greedy.findall(date)[0])
+            while len(work[1]) < 3:
+                work[1] += 'A'
+            return f'!{work[0].zfill(1)} {work[1].upper()} {work[2].zfill(3)}'
+
+        @staticmethod
+        def digits(date: list | tuple) -> tuple:
+            base26 = '0123456789abcdefghijklmnop'
+            if len(date) >= 6:
+                # Handle for when 4 or more Letters were provided; !1234 BAAB 123 -> [1234, 1, 0, 0, 1, 123]
+                date = list(date)
+                date[0] += int(''.join(base26[d] for d in date[1:-4]) + '0', 26)
+                del date[1:-4]
+            return tuple(date)
+
+
+# ============== CC Date Class ===============
+@dataclasses.dataclass
+class cc_date:
+    """
+    Docstring to be implemented\n
+    -Blob
+     """
+    date: list | tuple | str | int  # Always convert to int
+    radixes: list | tuple = dataclasses.field(default_factory=list)
+    weights: list | tuple = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        assert type(self.date) in (list, tuple, str, int), f'Date must be list, tuple, str or int'
+        assert type(self.weights) in (list, tuple), f'Weights must be list or tuple'
+        assert type(self.radixes) in (list, tuple), f'Radixes must be list or tuple'
+
+        # Normalize & Convert to Digits Date
+        if type(self.date) in (list, tuple):
+            self.date = cc_format.normalize.digits(self.date)
+        elif type(self.date) is str:
+            self.date = cc_format.normalize.string(self.date)
+            self.date = convert.string_to_digits(self.date)
+        elif type(self.date) is int:
+            self.date = convert.decimal_to_digits(self.date)
+
+        # Use Digits Date to calculate Radix & Weights
+        if not self.radixes:
+            self.radixes = calc.radixes(self.date)
+        if not self.weights:
+            self.weights = calc.weights(self.radixes)
+
+        # Make date Decimal Date
+        self.date = convert.digits_to_decimal(self.date, self.weights)
+
+    @property
+    def decimal(self) -> int:
+        """
+        :return: CC Date in Decimal format
+        """
+        assert type(self.date) is int, \
+            "cc_date.date shouldn't be modified after initialization. Must stay of an int."
+        return self.date
+
+    @property
+    def digits(self) -> tuple:
+        """
+        :return: CC Date in Digits format
+        """
+        assert type(self.date) is int, \
+            "cc_date.date shouldn't be modified after initialization. Must stay of an int."
+        return convert.decimal_to_digits(self.date)
+
+    @property
+    def string(self) -> str:
+        """
+        :return: CC Date in String format
+        """
+        assert type(self.date) is int, \
+            "cc_date.date shouldn't be modified after initialization. Must stay of an int."
+        return convert.decimal_to_string(self.date)
+
+
+# ============ Calculation Class ============
+class calc:
+    """
+    Docstring to be implemented\n
+    -Blob
+    """
+    @staticmethod
+    def radixes(date: list | tuple) -> tuple:
+        """
+        :param date: CC Date in Digits format
+        :return: Radixes of given date
+        """
+        return 10 ** len(repr(date[0])), 26, 26, 26, 1000
+
+    @staticmethod
+    def weights(radixes: list | tuple) -> tuple:
+        """
+        :param radixes: Radixes of a date
+        :return:
+        """
+        work, acc = [1], 1
+        for _radix in radixes[1:][::-1]:
+            # Calc Weights / Place Values
+            work.append(acc := acc * _radix)
+        return tuple(work[::-1])
+
+
+# ============= Conversion Class =============
+class convert:
+    """
+    Docstring to be implemented\n
+    -Blob
+     """
+    @staticmethod
+    def digits_to_decimal(date: list | tuple,
+                          weights: list | tuple = ...) -> int:
+        # Mixed to Base10
+        # NEEDS to be normalized with cc_format.normalize.digits() before passing to this func
+        if weights is Ellipsis or not weights:
+            weights = calc.weights(calc.radixes(date))
+        return sum(map(lambda weight, n: weight * n, weights, date))
+
+    @staticmethod
+    def decimal_to_digits(date: int) -> tuple:
+        # Base10 to Mixed
+        work = []
+        for r in (26, 26, 26, 1000)[::-1]:
+            # Loop through radixes. Excluding 5th radix.
+            work.append(date % r)
+            date //= r
+        # Append what's left. Same as if 5th radix is used.
+        work.append(date)
+        return tuple(work[::-1])
+
+    @staticmethod
+    def digits_to_string(date: list | tuple) -> str:
+        # NEEDS to be normalized with cc_format.normalize.digits() before passing to this func
+        return f"!{date[0]} {ascii_uppercase[date[1]]}{ascii_uppercase[date[2]]}{ascii_uppercase[date[3]]} {date[4]}"
+
+    @staticmethod
+    def string_to_digits(date: str) -> tuple:
+        # NEEDS to be normalized with cc_format.normalize.string() before passing to this func
+        date = date.lstrip('!').split(' ')
+        work = [int(date[0])]
+
+        for letter in date[1]:
+            work.append(ascii_uppercase.index(letter.upper()))
+
+        work.append(int(date[2]))
+        return cc_format.normalize.digits(work)
+
+    @staticmethod
+    def decimal_to_string(date: int) -> str:
+        work = convert.decimal_to_digits(date)
+        return convert.digits_to_string(work)
+
+    @staticmethod
+    def string_to_decimal(date: str, weights: list | tuple = ...) -> int:
+        # NEEDS to be normalized with cc_format.normalize.string() before passing to this func
+        work = convert.string_to_digits(date)
+        return convert.digits_to_decimal(work, weights)
+
+
+# ================ Math Stuff ================
+class cc_math:
+    @staticmethod
+    def add(dates: list[cc_date, ...] | tuple[cc_date, ...]) -> cc_date:
+        """
+        Adds CC Dates
+        :param dates: list or tuple of cc_date
+        :return:
+        """
+        assert len(dates) > 1
+        ans = 0
+        for date in dates:
+            ans += date.decimal
+        return cc_date(ans)
+
+    @staticmethod
+    def sub(dates: list[cc_date, ...] | tuple[cc_date, ...]) -> cc_date:
+        """
+        Subtracts CC Dates
+        :param dates: list or tuple of cc_date
+        :return:
+        """
+        assert len(dates) > 1
+        ans = dates[0].decimal
+        for date_ in dates[1:]:
+            ans -= date_.decimal
+        return cc_date(ans)
+
+    @staticmethod
+    def mul(dates: list[cc_date, ...] | tuple[cc_date, ...]) -> cc_date:
+        """
+        Multiples CC Dates
+        :param dates: list or tuple of cc_date
+        :return:
+        """
+        assert len(dates) > 1
+        ans = 1
+        for date_ in dates:
+            ans *= date_.decimal
+        return cc_date(ans)
+
+    @staticmethod
+    def div(dates: list[cc_date, ...] | tuple[cc_date, ...]) -> cc_date:
+        """
+        Floor Divides CC Dates
+        :param dates: list or tuple of cc_date
+        :return:
+        """
+        assert len(dates) > 1
+        ans = dates[0].decimal
+        for date_ in dates[1:]:
+            ans //= date_.decimal
+        return cc_date(ans)
+
+
+def cli():
+    """Used only for Command Line Tool"""
+    if __name__ != '__main__':
+        raise Exception("For CLI use only. ")
+
+    import argparse
+    from datetime import datetime
+    parser = argparse.ArgumentParser(description="For use with Ivycomb's Cosmic Critters date format.")
+
+    operation = parser.add_mutually_exclusive_group(required=True)
+    operation.add_argument('-a', '--add', help='Add 2 or more CC Dates', action='store_true')
+    operation.add_argument('-s', '--sub', help='Sub 2 or more CC Dates', action='store_true')
+    operation.add_argument('-m', '--multiple', help='Multiple 2 or more CC Dates', action='store_true')
+    operation.add_argument('-d', '--divide', help='Floor Divide 2 or more CC Dates', action='store_true')
+
+    operation.add_argument('-f', '--valid-formats', help='Prints all valid input formats', action='store_true')
+
+    operation.add_argument('-c', '--convert', choices=['string', 'digits', 'decimal', 'human'],
+                           help='Convert from String, Digits & Decimal Dates to String, Digits, Decimal or Human Dates')
+    operation.add_argument('-C', '--calculate', choices=['radix', 'weights'],
+                           help='Calculates the Radixes or Weights of given Date\\s')
+
+    parser.add_argument('dates', help='CC Date/s', nargs='+')
+    # parser.print_help()
+    args = parser.parse_args(['-f', '!1234 ABC 123', '!1234 ABC 123'])
+    # print(args)
+
+    if args.add:
+        print(cc_math.add([cc_date(date) for date in args.dates]).string)
+    elif args.sub:
+        print(cc_math.sub([cc_date(date) for date in args.dates]).string)
+    elif args.multiple:
+        print(cc_math.mul([cc_date(date) for date in args.dates]).string)
+    elif args.divide:
+        print(cc_math.div([cc_date(date) for date in args.dates]).string)
+
+    elif args.valid_formats:
+        print(f'Valid Input Formats:\n'
+              f'String & Decimal:\n{cc_format.valid_string_formats}\n\n'
+              f'Digits:\n{cc_format.valid_digits_format}')
+
+    elif args.convert == 'string':
+        print("Date\\s converted to String format:")
+        converted = [cc_date(date).string for date in args.dates]
+        for v, c in zip(args.dates, converted):
+            print(f'{v} -> {c}')
+
+    elif args.convert == 'digits':
+        print("Date\\s converted to Digits format:")
+        converted = [cc_date(date).digits for date in args.dates]
+        for v, c in zip(args.dates, converted):
+            print(f'{v} -> {c}')
+
+    elif args.convert == 'decimal':
+        print("Date\\s converted to Decimal format:")
+        converted = [cc_date(date).decimal for date in args.dates]
+        for v, c in zip(args.dates, converted):
+            print(f'{v} -> {c}')
+
+    elif args.convert == 'human':
+        print("Date\\s converted to Human format:")
+        converted = [datetime.fromtimestamp(cc_date(date).decimal) for date in args.dates]
+        for v, c in zip(args.dates, converted):
+            print(f'{v} -> {c}')
+
+    elif args.calculate == 'radix':
+        print("Place value Radixes:")
+        radixes = [cc_date(date).radixes for date in args.dates]
+        for v, r in zip(args.dates, radixes):
+            print(f'{v} -> {r}')
+
+    elif args.calculate == 'weights':
+        print("Place value Weights:")
+        weights = [cc_date(date).weights for date in args.dates]
+        for v, w in zip(args.dates, weights):
+            print(f'{v} -> {w}')
+
+    else:
+        raise Exception("Do look at me. Argparse must of decide to go poof.")
+
+
+if __name__ == '__main__':
+    import sys
+    print(cc_date('!1234 ABC 123').date)
+    # cli()
+    sys.exit(0)
